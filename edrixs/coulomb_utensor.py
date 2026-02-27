e6c0a25f8f0ba8860fd295491d4c7ca966682628
Mark Dean <mpmdean@gmail.com>
2026-02-27 16:29:39 -0500

    | > rather than bra

--- edrixs/coulomb_utensor.py
+++ edrixs/coulomb_utensor.py
@@ -84,8 +84,8 @@ def umat_slater(l_list, fk):
     
     .. math::
 
-        \\bra{i j}U_{m_{l_i}m_{s_i}, m_{l_j}m_{s_j}, m_{l_t}m_{s_t},
-        m_{l_u}m_{s_u}}^{i,j,t,u}\\ket{t u}
+        \\lange i j | U_{m_{l_i}m_{s_i}, m_{l_j}m_{s_j}, m_{l_t}m_{s_t},
+        m_{l_u}m_{s_u}}^{i,j,t,u} |t u \\rangle
 
     which is parameterized by
     Slater integrals :math:`F^{k}`:
